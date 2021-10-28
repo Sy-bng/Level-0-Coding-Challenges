1d86ba25714d559ad9276ecc103c13e456256a17
Sy-bng <76635480+Sy-bng@users.noreply.github.com>
2021-10-28 15:42:11 +0200

    Update Task05.py

--- Task05.py
+++ Task05.py
@@ -1,5 +1,5 @@
-a = int(input())
-b= int(input())
-c = int(input())
-area = ((s := (a+b+c) /2) *(s -a)*(s-b)*(s-c))**0.5 
+a = int(3)
+b= int(4)
+c = int(5)
+def_area = ((s := (a+b+c) /2) *(s -a)*(s-b)*(s-c))**0.5 
 print(area)
